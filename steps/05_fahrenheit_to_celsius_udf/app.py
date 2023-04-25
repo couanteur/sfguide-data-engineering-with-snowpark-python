b634f3b2b898ee8eb34797a34d11d2689ccdaac9
couanteur <49252324+couanteur@users.noreply.github.com>
2023-04-25 17:03:02 +0000

    change1

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -21,8 +21,10 @@ def main(temp_f: float) -> float:
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
-#if __name__ == '__main__':
- #   if len(sys.argv) > 1:
-  #      print(main(*sys.argv[1:]))  # type: ignore
-   # else:
-    #   print(main())  # type: ignore
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print(main(*sys.argv[1:]))  
+        #type: ignore
+    else:
+       print(main())  
+       #type: ignore
